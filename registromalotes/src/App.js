0d081b1b8685c67282667b862e2aa97ee92a04fe
Gabriel Formigheri Matte <gabrielmatte4@gmail.com>
2021-11-23 01:10:14 -0300

    Ajustes de services e variaveis do Firebase

--- registromalotes/src/App.js
+++ registromalotes/src/App.js
@@ -1,9 +1,80 @@
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import React, {useState, useLayoutEffect} from 'react'
+import {storageSave,storageRemove,storageGet} from "./services/storage"
 
 function App() {
+  const [lembreme, setLembreme] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  useLayoutEffect(()=>{
+    let emailStorage = storageGet("email")
+    if(emailStorage){
+      setEmail(emailStorage)
+    }
+
+  },[])
+
+  const handleLembreme = (e)=> {
+   setLembreme(e.target.checked)
+   if(e.target.checked == true){
+     storageSave("email",email)
+   }else{
+     storageRemove("email")
+   }
+
+  }
+
+
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={3}>
+      </Grid>
+      <Grid item xs={6}>
+      <TextField 
+      type="email" 
+      id="outlined-basic" 
+      label="E-mail" 
+      variant="outlined" 
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+
+      />
+      </Grid>
+      <Grid item xs={3}>
+      </Grid>
+      <Grid item xs={3}>
+      </Grid>
+      <Grid item xs={6}>
+      <TextField type="password" 
+      id="outlined-basic" 
+      label="Password" 
+      variant="outlined"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+ 
+      />
+      </Grid>
+      <Grid item xs={3}>
+      </Grid>
+      <Grid item xs={3}>
+      </Grid>
+      <Grid item xs={6}>
+      <FormGroup>
+      <FormControlLabel control={<Checkbox checked={lembreme} onChange={handleLembreme} />} label="Lembre-me" />
+      </FormGroup>
+      <Button variant="contained" size="small">
+          Login
+        </Button>
+      <Grid item xs={3}> </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ registromalotes/src/services/Firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8pOLZmxRitogQRYN1rrVwjLXzBbnjJVY",
+  authDomain: "cadastromalotes.firebaseapp.com",
+  projectId: "cadastromalotes",
+  storageBucket: "cadastromalotes.appspot.com",
+  messagingSenderId: "732820600858",
+  appId: "1:732820600858:web:0c6583b8e89fab442479a3"
+};
+
+
+const app = initializeApp(firebaseConfig);
